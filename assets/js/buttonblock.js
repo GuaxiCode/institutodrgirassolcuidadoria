0d1d97ecffaddd3dc9420d01fe101744206614f9
GuaxiCode <guaxi.dev@gmail.com>
2022-06-10 23:17:50 -0300

    Update buttonblock.js

--- assets/js/buttonblock.js
+++ assets/js/buttonblock.js
@@ -3,3 +3,9 @@ function protegercodigo() {
             alert('Botão bloqueado.');}
             }
             document.onmousedown=protegercodigo
+
+document.onkeydown = function(e) {
+    if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 ||    e.keyCode === 117 || e.keycode === 17 || e.keycode === 85)) {
+        alert('Não permitido!');}
+            return false;
+};
